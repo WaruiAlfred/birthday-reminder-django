401cab323b4a9c2232d3a1984702b50a1d72748a
WaruiAlfred <alfredwarui17@gmail.com>
2022-05-02 17:21:57 +0300

    create birthday model

--- reminder/models.py
+++ reminder/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Birthdays(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    friends_name = models.CharField(max_length=100)
+    friends_birthdate = models.DateField()
+    current_date = models.DateField(auto_now_add=True)
+    # year_of_birth = models.IntegerField(max_length=4)
+    # month_of_birth = models.IntegerField(max_length=2)
+    # day_of_birth = models.IntegerField(max_length=2)
+
+    def __str__(self):
+        return self.friends_name
