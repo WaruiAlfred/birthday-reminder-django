4355ba6abcd7886bece599050352f4c6acba9a9e
WaruiAlfred <alfredwarui17@gmail.com>
2022-05-03 00:53:37 +0300

    define birthday display fields in admin page

--- reminder/admin.py
+++ reminder/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Birthdays
 
-# Register your models here.
+
+class BirthdaysAdmin(admin.ModelAdmin):
+    list_display = Birthdays.DisplayFields
+    list_filter = (
+        "user",
+        "friends_name",
+        "friends_birthdate",
+    )
+
+
+admin.site.register(Birthdays, BirthdaysAdmin)
