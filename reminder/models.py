ec96e0a4c6a56ad5485f508949af388d83dde7c1
WaruiAlfred <alfredwarui17@gmail.com>
2022-05-03 00:52:38 +0300

    alter birthday model

--- reminder/models.py
+++ reminder/models.py
@@ -3,10 +3,11 @@ from django.contrib.auth.models import User
 
 
 class Birthdays(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name="users", on_delete=models.CASCADE)
     friends_name = models.CharField(max_length=100)
     friends_birthdate = models.DateField()
     current_date = models.DateField(auto_now_add=True)
+    DisplayFields = ["id", "user", "friends_name", "friends_birthdate", "current_date"]
     # year_of_birth = models.IntegerField(max_length=4)
     # month_of_birth = models.IntegerField(max_length=2)
     # day_of_birth = models.IntegerField(max_length=2)
