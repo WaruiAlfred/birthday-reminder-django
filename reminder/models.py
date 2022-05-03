173d98c3d1704ba614d9eb3969a220c36b4c9530
WaruiAlfred <alfredwarui17@gmail.com>
2022-05-03 16:55:58 +0300

    change birthday entry fields

--- reminder/models.py
+++ reminder/models.py
@@ -5,12 +5,17 @@ from django.contrib.auth.models import User
 class Birthdays(models.Model):
     user = models.ForeignKey(User, related_name="users", on_delete=models.CASCADE)
     friends_name = models.CharField(max_length=100)
-    friends_birthdate = models.DateField()
-    current_date = models.DateField(auto_now_add=True)
-    DisplayFields = ["id", "user", "friends_name", "friends_birthdate", "current_date"]
-    # year_of_birth = models.IntegerField(max_length=4)
-    # month_of_birth = models.IntegerField(max_length=2)
-    # day_of_birth = models.IntegerField(max_length=2)
+    year_of_birth = models.IntegerField(default=2000)
+    month_of_birth = models.IntegerField(default=11)
+    day_of_birth = models.IntegerField(default=12)
+    DisplayFields = [
+        "id",
+        "user",
+        "friends_name",
+        "year_of_birth",
+        "month_of_birth",
+        "day_of_birth",
+    ]
 
     def __str__(self):
         return self.friends_name
