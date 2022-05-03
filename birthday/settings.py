f502011be34c3e4d279b45bba38e0956c6508a81
WaruiAlfred <alfredwarui17@gmail.com>
2022-05-03 23:11:05 +0300

    configure swagger for API documentation

--- birthday/settings.py
+++ birthday/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'reminder',
     'rest_framework',
     'rest_framework_simplejwt',
+    'drf_yasg',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
