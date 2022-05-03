16428344590a8e183c22738f60aab6c9b8b9a11e
WaruiAlfred <alfredwarui17@gmail.com>
2022-05-03 16:56:44 +0300

    create reminder view function

--- reminder/views.py
+++ reminder/views.py
@@ -1,7 +1,13 @@
 from .models import Birthdays
+from .email import send_reminder_mail
 from .serializers import BirthdaySerializer, BirthdayDataSerializer
+
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, schema
+
+import datetime
 
 
 class BirthdaysView(viewsets.ModelViewSet):
@@ -17,3 +23,27 @@ class BirthdaysDataView(viewsets.ModelViewSet):
     queryset = Birthdays.objects.all()
     serializer_class = BirthdayDataSerializer
     permission_classes = (IsAuthenticatedOrReadOnly,)
+
+@api_view(["GET"])
+@schema(None)
+def birthday_reminder(request):
+    current_date = datetime.datetime.utcnow().strftime("%m-%d")
+    message = "Today is not your friend(s) birthday."
+    birthdays = Birthdays.objects.filter(user=request.user.id).all()
+
+    if birthdays.exists():
+        for birthday in birthdays:
+            birthdate = datetime.datetime(
+                birthday.year_of_birth, birthday.month_of_birth, birthday.day_of_birth
+            ).strftime("%m-%d")
+            if current_date == birthdate:
+                send_reminder_mail(
+                    request.user.username, birthday.friends_name, request.user.email
+                )
+                message = "Reminder sent"
+            else:
+                message = "Your friend(s) birthday is due."
+    else:
+        message = "You have no birthday entries.Enter some then try again"
+
+    return Response({"message": message})
